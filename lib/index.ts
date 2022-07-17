a1b8740fde9ea158ab6977f64cf35ef08f992b1e
Sami Salih İbrahimbas <info@ssibrahimbas.com>
2022-07-17 20:06:26 +0300

    core: http-status added

--- lib/index.ts
+++ lib/index.ts
@@ -10,3 +10,4 @@ export * from "./set/unique.set";
 export * from "./set/readonly.set";
 export * from "./map/readonly.map";
 export * from "./status/status-codes";
+export * from "./types/http-status.types";
